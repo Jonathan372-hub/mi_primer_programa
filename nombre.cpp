e4760735f794de1094d0f61e37072c6b473e89ac
Jonathan372-hub <mondragonjonthan71@gmail.com>
2025-03-25 13:15:08 -0600

    Mi primer commit

---
+++ nombre.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+int main(){
+	cout << "*****   \n";
+	cout << "*    *  \n";
+	cout << "*    *   \n";
+	cout << "*****     \n";
+	cout << "Presione enter para finalizar";
+	cin.get();
+	return 0;
+}
